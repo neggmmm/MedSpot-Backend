7cfe30a2511871a2b2d65a9e13435bac3ee16919
neggmmm <kekkdead@gmail.com>
2026-05-19 16:46:42 +0300

    add logger services with warn, info and error for logging

---
+++ src/common/logger/logger.service.ts
@@ -0,0 +1,69 @@
+import {
+ Injectable
+} from '@nestjs/common';
+
+import {
+ PinoLogger
+} from 'nestjs-pino';
+
+@Injectable()
+
+export class AppLogger {
+
+ constructor(
+   private readonly logger:
+   PinoLogger
+ ) {}
+
+ info(
+   context:string,
+   message:string,
+   meta?:object
+ ){
+
+   this.logger.setContext(
+     context
+   );
+
+   this.logger.info(
+     meta ?? {},
+     message
+   );
+
+ }
+
+ warn(
+   context:string,
+   message:string,
+   meta?:object
+ ){
+
+   this.logger.setContext(
+     context
+   );
+
+   this.logger.warn(
+     meta ?? {},
+     message
+   );
+
+ }
+
+ error(
+   context:string,
+   message:string,
+   error?:unknown
+ ){
+
+   this.logger.setContext(
+     context
+   );
+
+   this.logger.error(
+     { error },
+     message
+   );
+
+ }
+
+}
